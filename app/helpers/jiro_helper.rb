aa030e3d1ebf709921dbe9f8f5f782470cf28022
Zenichiro Ishikawa <zenichiro0419@gmail.com>
2021-01-04 03:18:38 +0900

    基本情報：コールタイミング及び店内設備の表追加

--- app/helpers/jiro_helper.rb
+++ app/helpers/jiro_helper.rb
@@ -1,7 +1,15 @@
 module JiroHelper
   # @return[string]
   def exist(facility)
+    return '-' if facility.nil?
     return '○' if facility
     return '×' unless facility
+  end
+
+  # @return[string]
+  def show_waiting_seat(seat_count)
+    return "-" if seat_count.nil?
+    return "○(#{seat_count})" if seat_count > 0
+    return "×" if seat_count.zero?
   end
 end
