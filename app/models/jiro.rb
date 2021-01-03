2f6f9558c652fa369a1123c6a52fab0e5eddcfe4
Zenichiro Ishikawa <zenichiro0419@gmail.com>
2021-01-04 03:16:59 +0900

    enumの追加及び一部カラムの初期値追加

--- app/models/jiro.rb
+++ app/models/jiro.rb
@@ -29,7 +29,15 @@
 class Jiro < ApplicationRecord
   enum payment_method: { default: 0, only_cash: 1, available_bisides_cash: 2 }, _prefix: true
   enum how_to_order: { default: 0, meal_voucher_system: 1, order_system: 2}, _prefix: true
+  enum call_timing: { default: 0, submit_meal_voucher: 1, boiled_noodles: 2}, _prefix: true
 
   attribute :payment_method, :integer, default: 0
   attribute :how_to_order, :integer, default: 0
+  attribute :is_apron, :boolean, default: nil
+  attribute :is_parking_area, :boolean, default: nil
+  attribute :is_renge, :boolean, default: nil
+  attribute :is_rest_room, :boolean, default: nil
+  attribute :is_tissue, :boolean, default: nil       
+  attribute :is_trash_box, :boolean, default: nil       
+  attribute :is_water_server, :boolean, default: nil
 end
