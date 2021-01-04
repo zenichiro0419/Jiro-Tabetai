16f1478f291954164d09bb7b8c5db0d3c959af33
Zenichiro Ishikawa <zenichiro0419@gmail.com>
2021-01-04 11:38:54 +0900

    RDOC修正

--- app/helpers/jiro_helper.rb
+++ app/helpers/jiro_helper.rb
@@ -1,15 +1,17 @@
 module JiroHelper
-  # @return[string]
+  # @params [boolean] facility
+  # @return [string]
   def exist(facility)
-    return '-' if facility.nil?
+    return '未設定' if facility.nil?
     return '○' if facility
     return '×' unless facility
   end
 
-  # @return[string]
+  # @params [integer] seat_count
+  # @return [string]
   def show_waiting_seat(seat_count)
-    return "-" if seat_count.nil?
+    return '未設定' if seat_count.nil?
     return "○(#{seat_count})" if seat_count > 0
-    return "×" if seat_count.zero?
+    return '×' if seat_count.zero?
   end
 end
