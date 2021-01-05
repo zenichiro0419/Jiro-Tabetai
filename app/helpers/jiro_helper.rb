dc24611323cd01d7c4064ffb2e8e71124e5aaed3
Zenichiro Ishikawa <zenichiro0419@gmail.com>
2021-01-06 04:14:59 +0900

    卓上テーブル

--- app/helpers/jiro_helper.rb
+++ app/helpers/jiro_helper.rb
@@ -11,7 +11,19 @@ module JiroHelper
   # @return [string]
   def show_waiting_seat(seat_count)
     return '未設定' if seat_count.nil?
-    return "○(#{seat_count})" if seat_count > 0
+    return "○(#{seat_count})" if seat_count.positive?
     return '×' if seat_count.zero?
+  end
+
+  # @params [Array] table_seasonings
+  # @return [string] list
+  def table_seasoning_list(table_seasonings)
+    list = ''
+    if table_seasonings.blank?
+      list = '未設定'
+    else
+      table_seasonings.each { |seasoning| list += "#{seasoning} / " }
+      list
+    end
   end
 end
