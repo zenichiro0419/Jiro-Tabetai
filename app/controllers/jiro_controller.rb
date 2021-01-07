ef5fe7dcf5f8929511c3dfd266d71a721867496e
Zenichiro Ishikawa <zenichiro0419@gmail.com>
2021-01-08 01:45:21 +0900

    営業時間(BusinessHour)のデータ整形をするメソッド追加

--- app/controllers/jiro_controller.rb
+++ app/controllers/jiro_controller.rb
@@ -5,6 +5,8 @@ class JiroController < ApplicationController
     @main_menus = @jiro.main_menu
     @option_menus = @jiro.option_menu
     @toppings_list = create_toppings_list(@main_menus.pluck(:id))
+    business_hours = @jiro.business_hour.group_by(&:wday)
+    @business_hour_list = create_business_hour_list(business_hours)
   end
 
   private
@@ -24,5 +26,19 @@ class JiroController < ApplicationController
       toppings_list.store(main_menu_id, topping_names)
     end
     toppings_list
+  end
+
+  # @params [Hash] business_hours
+  # @return [Hash] business_hour_list => [integer] key, [Hash] value
+  def create_business_hour_list(business_hours)
+    business_hour_list = {}
+    (0..6).each do |wday|
+      jiro_open_list = {}
+      business_hours[wday].each do |business_hour|
+        jiro_open_list.store(business_hour.category, [business_hour.start_at, business_hour.end_at])
+      end
+      business_hour_list.store(wday, jiro_open_list)
+    end
+    business_hour_list
   end
 end
