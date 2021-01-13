16bcfe9cc97f7b3f6ace51a33c8572104992e59b
Zenichiro Ishikawa <zenichiro0419@gmail.com>
2021-01-13 22:34:57 +0900

    controller:タイポ修正

--- app/controllers/jiros_controller.rb
+++ app/controllers/jiros_controller.rb
@@ -2,10 +2,10 @@ class JirosController < ApplicationController
   def show
     @jiro = Jiro.find(params[:id])
     @facility = @jiro.facility
-    @table_seasonings = [@facility.seasoning1, @facility.seasoning2, @facility.seasoning3, @facility.seasoning4, @facility.seasoning5,
-                         @facility.seasoning6]
-    @main_menus = jiro.menu.main_menu
-    @option_menus = jiro.menu.option_menu
+    @table_seasonings = %w[@facility.seasoning1 @facility.seasoning2 @facility.seasoning3 @facility.seasoning4 @facility.seasoning5
+                           @facility.seasoning6]
+    @main_menus = @jiro.menu.main_menu
+    @option_menus = @jiro.menu.option_menu
     business_hours = @jiro.business_hour.group_by(&:wday)
     @business_hour_list = create_business_hour_list(business_hours) if business_hours.present?
   end
