a38d58f8917d11c2c474486cedf4a942750a3684
Zenichiro Ishikawa <zenichiro0419@gmail.com>
2021-01-13 22:08:04 +0900

    menuのインスタンス名修正

--- app/controllers/jiros_controller.rb
+++ app/controllers/jiros_controller.rb
@@ -4,8 +4,8 @@ class JirosController < ApplicationController
     @facility = @jiro.facility
     @table_seasonings = [@facility.seasoning1, @facility.seasoning2, @facility.seasoning3, @facility.seasoning4, @facility.seasoning5,
                          @facility.seasoning6]
-    @main_menu = jiro.menu.main_menu
-    @option_menu = jiro.menu.option_menu
+    @main_menus = jiro.menu.main_menu
+    @option_menus = jiro.menu.option_menu
     business_hours = @jiro.business_hour.group_by(&:wday)
     @business_hour_list = create_business_hour_list(business_hours) if business_hours.present?
   end
