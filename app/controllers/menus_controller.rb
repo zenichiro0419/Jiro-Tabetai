57543300df9190523a79615425b14233d804fdd3
Zenichiro Ishikawa <zenichiro0419@gmail.com>
2021-01-16 03:12:55 +0900

    wip

--- app/controllers/menus_controller.rb
+++ app/controllers/menus_controller.rb
@@ -1,2 +1,7 @@
 class MenusController < ApplicationController
+  def edit
+    @menu = Menu.where(jiro_id: params[:jiro_id])
+  end
+
+  def update; end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   resources :jiros, except: [:destroy] do
+    resource :business_hours, only: [:edit, :update]
     resource :facilities, only: [:edit, :update]
+    resource :menus, only: [:edit, :update]
   end
 end
