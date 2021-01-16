29cae93bb033cc9388d68aa527c9d1b64883a8f7
Zenichiro Ishikawa <zenichiro0419@gmail.com>
2021-01-16 21:44:38 +0900

    reset for PR

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,2 @@
 Rails.application.routes.draw do
-  resources :jiros, except: [:destroy] do
-    resource :business_hours, only: [:edit, :update]
-    resource :facilities, only: [:edit, :update]
-    resource :menus, only: [:edit, :update]
-  end
 end
