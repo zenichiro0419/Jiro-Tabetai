84bb8198f573b2f9a304263269136dad3244d631
Zenichiro Ishikawa <zenichiro0419@gmail.com>
2021-01-23 02:20:07 +0900

    フォームをメインメニューとオプションメニューで分ける

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,11 @@ Rails.application.routes.draw do
   resources :jiros, except: [:destroy] do
     resource :business_hours, only: [:edit, :update]
     resource :facilities, only: [:edit, :update]
-    resource :menu_items, only: [:edit, :update]
+    resource :menu_items, only: [:edit] do
+      member do
+        patch 'update_main_menu'
+        patch 'update_option_menu'
+      end
+    end
   end
 end
