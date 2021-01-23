7f80e911e202d830e23f2534720c991556ab0959
Zenichiro Ishikawa <zenichiro0419@gmail.com>
2021-01-23 13:18:18 +0900

    更新カラムの配列格納をprivateに切り出し(BusinessHour)

--- app/controllers/business_hours_controller.rb
+++ app/controllers/business_hours_controller.rb
@@ -6,13 +6,8 @@ class BusinessHoursController < ApplicationController
   def update
     @jiro = Jiro.find_by_id(params[:jiro_id])
     @business_hours = BusinessHour.where(jiro_id: params[:jiro_id])
-    update_business_hours = []
-    @business_hours.each do |business_hour|
-      business_hour.is_holiday = business_hours_params(business_hour.id)[:is_holiday]
-      business_hour.start_at = business_hours_params(business_hour.id)[:start_at]
-      business_hour.end_at = business_hours_params(business_hour.id)[:end_at]
-      update_business_hours << business_hour
-    end
+
+    update_business_hours = push_update_business_hours(@business_hours)
 
     BusinessHour.transaction do
       BusinessHour.import(update_business_hours, on_duplicate_key_update: [:is_holiday, :start_at, :end_at])
@@ -23,6 +18,16 @@ class BusinessHoursController < ApplicationController
   end
 
   private
+
+  def push_update_business_hours(business_hours)
+    update_business_hours = []
+    business_hours.each do |business_hour|
+      business_hour.is_holiday = business_hours_params(business_hour.id)[:is_holiday]
+      business_hour.start_at = business_hours_params(business_hour.id)[:start_at]
+      business_hour.end_at = business_hours_params(business_hour.id)[:end_at]
+      update_business_hours << business_hour
+    end
+  end
 
   def business_hours_params(business_hour_id)
     params.require(:business_hour).require(:business_hour).require(business_hour_id.to_s)
