f7e0e42bae506eea08072e38e5a8de9ac492f9e7
Zenichiro Ishikawa <zenichiro0419@gmail.com>
2021-01-23 13:12:12 +0900

    更新カラムを格納する部分をprivateに切り出し

--- app/controllers/menu_items_controller.rb
+++ app/controllers/menu_items_controller.rb
@@ -10,21 +10,7 @@ class MenuItemsController < ApplicationController
     menu_items = MenuItem.where(jiro_id: params[:jiro_id])
 
     @main_menu_items = menu_items.main_menu
-    update_main_menu_items = []
-    @main_menu_items.each do |main_menu_item|
-      main_menu_item.name = main_menu_items_params(main_menu_item.id)[:name]
-      main_menu_item.price = main_menu_items_params(main_menu_item.id)[:price]
-      main_menu_item.noodle_quantity = main_menu_items_params(main_menu_item.id)[:noodle_quantity]
-      main_menu_item.note = main_menu_items_params(main_menu_item.id)[:note]
-      main_menu_item.topping1 = main_menu_items_params(main_menu_item.id)[:topping1]
-      main_menu_item.topping2 = main_menu_items_params(main_menu_item.id)[:topping2]
-      main_menu_item.topping3 = main_menu_items_params(main_menu_item.id)[:topping3]
-      main_menu_item.topping4 = main_menu_items_params(main_menu_item.id)[:topping4]
-      main_menu_item.topping5 = main_menu_items_params(main_menu_item.id)[:topping5]
-      main_menu_item.topping6 = main_menu_items_params(main_menu_item.id)[:topping6]
-      main_menu_item.topping7 = main_menu_items_params(main_menu_item.id)[:topping7]
-      update_main_menu_items << main_menu_item
-    end
+    update_main_menu_items = push_update_main_menu_items(@main_menu_items)
 
     MenuItem.transaction do
       MenuItem.import(update_main_menu_items,
@@ -41,12 +27,7 @@ class MenuItemsController < ApplicationController
     menu_items = MenuItem.where(jiro_id: params[:jiro_id])
 
     @option_menu_items = menu_items.option_menu
-    update_option_menu_items = []
-    @option_menu_items.each do |option_menu_item|
-      option_menu_item.name = option_menu_items_params(option_menu_item.id)[:name]
-      option_menu_item.price = option_menu_items_params(option_menu_item.id)[:price]
-      update_option_menu_items << option_menu_item
-    end
+    update_option_menu_items = push_update_option_menu_items(@option_menu_items)
 
     MenuItem.transaction do
       MenuItem.import(update_option_menu_items, on_duplicate_key_update: [:name, :price])
@@ -57,6 +38,33 @@ class MenuItemsController < ApplicationController
   end
 
   private
+
+  def push_update_main_menu_items(main_menu_items)
+    update_main_menu_items = []
+    main_menu_items.each do |main_menu_item|
+      main_menu_item.name = main_menu_items_params(main_menu_item.id)[:name]
+      main_menu_item.price = main_menu_items_params(main_menu_item.id)[:price]
+      main_menu_item.noodle_quantity = main_menu_items_params(main_menu_item.id)[:noodle_quantity]
+      main_menu_item.note = main_menu_items_params(main_menu_item.id)[:note]
+      main_menu_item.topping1 = main_menu_items_params(main_menu_item.id)[:topping1]
+      main_menu_item.topping2 = main_menu_items_params(main_menu_item.id)[:topping2]
+      main_menu_item.topping3 = main_menu_items_params(main_menu_item.id)[:topping3]
+      main_menu_item.topping4 = main_menu_items_params(main_menu_item.id)[:topping4]
+      main_menu_item.topping5 = main_menu_items_params(main_menu_item.id)[:topping5]
+      main_menu_item.topping6 = main_menu_items_params(main_menu_item.id)[:topping6]
+      main_menu_item.topping7 = main_menu_items_params(main_menu_item.id)[:topping7]
+      update_main_menu_items << main_menu_item
+    end
+  end
+
+  def push_update_option_menu_items(option_menu_items)
+    update_option_menu_items = []
+    option_menu_items.each do |option_menu_item|
+      option_menu_item.name = option_menu_items_params(option_menu_item.id)[:name]
+      option_menu_item.price = option_menu_items_params(option_menu_item.id)[:price]
+      update_option_menu_items << option_menu_item
+    end
+  end
 
   def main_menu_items_params(menu_item_id)
     params.require(:menu_item).require(:main_menu_item).require(menu_item_id.to_s)
