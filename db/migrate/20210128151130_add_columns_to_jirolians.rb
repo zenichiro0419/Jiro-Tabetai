8009a690941d7d592d47b20296bf3c6792721c8d
Zenichiro Ishikawa <zenichiro0419@gmail.com>
2021-01-29 00:29:30 +0900

    Revert "no message"
    This reverts commit d5674ff321c0bf523ad1d3eed86f669579769d87.

--- db/migrate/20210128151130_add_columns_to_jirolians.rb
+++
@@ -1,7 +0,0 @@
-class AddColumnsToJirolians < ActiveRecord::Migration[6.0]
-  def change
-    add_column :jirolians, :provider, :string
-    add_column :jirolians, :uid, :string
-    add_column :jirolians, :username, :string
-  end
-end
