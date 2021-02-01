37a9733b82034f471b4e7e37d8ed5b689ce76cca
Zenichiro Ishikawa <zenichiro0419@gmail.com>
2021-02-02 02:43:56 +0900

    RSpecでdeviseを使用できるように設定

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -9,7 +9,6 @@ abort('The Rails environment is running in production mode!') if Rails.env.produ
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
 require 'devise'
-require 'support/controller_macros'
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
 # run as spec files by default. This means that files in spec/support that end
@@ -23,7 +22,7 @@ require 'support/controller_macros'
 # directory. Alternatively, in the individual `*_spec.rb` files, manually
 # require only the support files necessary.
 #
-# Dir[Rails.root.join('spec', 'support', '**', '*.rb')].each { |f| require f }
+Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 
 # Checks for pending migrations and applies them before tests are run.
 # If you are not using ActiveRecord, you can remove these lines.
@@ -64,7 +63,8 @@ RSpec.configure do |config|
 
   # NOTE: devise認証のための設定
   config.include Devise::Test::IntegrationHelpers, type: :request
-  config.extend ControllerMacros, type: :controller
+  config.include Devise::Test::ControllerHelpers, type: :controller
+  config.extend AuthenticationMacros
 
   # FactoryBotのシンタックスを省略する
   config.include FactoryBot::Syntax::Methods
