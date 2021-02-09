83648195434d025e3634d55b22c729b91a0ddf5a
Zenichiro Ishikawa <zenichiro0419@gmail.com>
2021-02-10 02:55:38 +0900

    jiro modelにeat系statueのメソッド追加

--- app/models/jiro.rb
+++ app/models/jiro.rb
@@ -33,4 +33,12 @@ class Jiro < ApplicationRecord
   validates :name, presence: true
   # VALID_PHONE_NUMBER_REGEX = /\A0(\d{1}[-(]?\d{4}|\d{2}[-(]?\d{3}|\d{3}[-(]?\d{2}|\d{4}[-(]?\d{1})[-)]?\d{4}\z|\A0[5789]0-?\d{4}-?\d{4}\z/.freeze
   # validates :phone_number, format: {with: VALID_PHONE_NUMBER_REGEX}
+
+  def checked_wanna_eat_by?(jirolian)
+    wanna_eat_statuses.where(jirolian_id: jirolian.id).exists?
+  end
+
+  def checked_have_eaten_by?(jirolian)
+    have_eaten_statuses.where(jirolian_id: jirolian.id).exists?
+  end
 end
