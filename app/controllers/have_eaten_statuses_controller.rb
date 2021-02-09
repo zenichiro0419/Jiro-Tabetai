607ff6a9fd6668fe5e55bd4a19909b3f326393a3
Zenichiro Ishikawa <zenichiro0419@gmail.com>
2021-02-10 02:01:42 +0900

    controllerにcreate/destroyメソッド追加

--- app/controllers/have_eaten_statuses_controller.rb
+++ app/controllers/have_eaten_statuses_controller.rb
@@ -1,2 +1,22 @@
 class HaveEatenStatusesController < ApplicationController
+  before_action :set_jiro
+  before_action :authenticate_jirolian!
+
+  def create
+    wanna_eat_status = @jiro.have_eaten_statuses.new(jirolian_id: current_jirolian.id)
+    have_eaten_statuse.save
+    redirect_to request.referer
+  end
+
+  def destroy
+    have_eaten_statuse = current_jirolian.have_eaten_statuses.find_by(jiro_id: @jiro.id)
+    have_eaten_statuse.destroy
+    redirect_to request.referer
+  end
+
+  private
+
+  def set_post
+    @jiro = Jiro.find(params[:jiro_id])
+  end
 end
--- app/controllers/wanna_eat_statuses_controller.rb
+++ app/controllers/wanna_eat_statuses_controller.rb
@@ -1,2 +1,22 @@
 class WannaEatStatusesController < ApplicationController
+  before_action :set_jiro
+  before_action :authenticate_jirolian!
+
+  def create
+    wanna_eat_status = @jiro.wanna_eat_statuses.new(jirolian_id: current_jirolian.id)
+    wanna_eat_statuse.save
+    redirect_to request.referer
+  end
+
+  def destroy
+    wanna_eat_statuse = current_jirolian.wanna_eat_statuses.find_by(jiro_id: @jiro.id)
+    wanna_eat_statuse.destroy
+    redirect_to request.referer
+  end
+
+  private
+
+  def set_post
+    @jiro = Jiro.find(params[:jiro_id])
+  end
 end
