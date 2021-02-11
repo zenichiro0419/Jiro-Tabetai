62ad8be0d0222ddcb4704a7bf7d06c20c167973c
Zenichiro Ishikawa <zenichiro0419@gmail.com>
2021-02-12 01:39:43 +0900

    検索機画面及び検索結果画面作成

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,3 +1,19 @@
 class TopController < ApplicationController
+  before_action :set_search
+
   def index; end
+
+  def search
+    @jiros = @search.result(distinct: true)
+    if @jiros.blank?
+      flash[:alert] = '該当する店舗が登録されていません。'
+      redirect_to root_path
+    end
+  end
+
+  private
+
+  def set_search
+    @search = Jiro.ransack(params[:q])
+  end
 end
