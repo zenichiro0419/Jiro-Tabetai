65391791fd9b61bf4cf53d1267209494b5a469b7
Zenichiro Ishikawa <zenichiro0419@gmail.com>
2021-02-25 03:36:56 +0900

    味のスペックをenum化及びそれらの日本語化

--- app/models/post.rb
+++ app/models/post.rb
@@ -20,6 +20,11 @@ class Post < ApplicationRecord
   belongs_to :jirolian
   belongs_to :jiro
 
+  enum soup: {emulsification: 1, little_emulsification: 2, non_emulsification: 3}
+  enum noodle: {soft: 1, little_soft: 2, little_hard: 3, hard: 4}
+  enum boiled_vegetable: {tender: 1, tough: 2}
+  enum roasted_pork: {crumbly: 1, melted: 2, fatty: 3}
+
   validates :content, presence: true
   validates :jiro_id, presence: true
   validates :jirolian_id, presence: true
