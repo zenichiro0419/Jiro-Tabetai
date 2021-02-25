aa38db368a10fbd5bda62cd205876aef2adb46a1
Zenichiro Ishikawa <zenichiro0419@gmail.com>
2021-02-25 12:27:46 +0900

    Afavボタンのjax対応

--- app/controllers/favorite_posts_controller.rb
+++ app/controllers/favorite_posts_controller.rb
@@ -5,14 +5,11 @@ class FavoritePostsController < ApplicationController
   def create
     favorite_post = @post.favorite_posts.new(jirolian_id: current_jirolian.id)
     favorite_post.save
-    flash[:success] = 'Liked post'
-    redirect_to request.referer
   end
 
   def destroy
     favorite_post = current_jirolian.favorite_posts.find_by(post_id: @post.id)
     favorite_post.destroy
-    redirect_to request.referer
   end
 
   private
